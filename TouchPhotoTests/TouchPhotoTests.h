e57b12d4d8b124836008a81da2f34b934705f0fe
Cong Vo <kong@beedream.net>
2012-02-21 14:41:46 +0700

    Initial Commit

---
+++ TouchPhotoTests/TouchPhotoTests.h
@@ -0,0 +1,13 @@
+//
+//  TouchPhotoTests.h
+//  TouchPhotoTests
+//
+//  Created by Vo Thanh Cong on 2/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TouchPhotoTests : SenTestCase
+
+@end
---
+++ TouchPhotoTests/TouchPhotoTests.m
@@ -0,0 +1,32 @@
+//
+//  TouchPhotoTests.m
+//  TouchPhotoTests
+//
+//  Created by Vo Thanh Cong on 2/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TouchPhotoTests.h"
+
+@implementation TouchPhotoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TouchPhotoTests");
+}
+
+@end
